18e7653449566daec586f4f5830829cdf25dea1f
petruha <refloader@gmail.com>
2023-11-07 20:29:55 +0300

    Add login form

---
+++ src/components/form/form-login/FormLogin.stories.tsx
@@ -0,0 +1,10 @@
+import FormLoginTranslated from './FormLogin';
+
+export default {
+  title: 'Login form',
+  component: FormLoginTranslated,
+};
+
+export const Default = {
+  args: {},
+};
---
+++ src/components/form/form-login/FormLogin.tsx
@@ -0,0 +1,82 @@
+import React, { ReactNode } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import * as yup from 'yup';
+import styled from 'styled-components';
+import { withTranslation } from 'react-i18next';
+import { yupResolver } from '@hookform/resolvers/yup';
+import Input from '../../input/Input';
+type Inputs = {
+  email: string;
+  password: string;
+};
+const FormProfileStyled = styled.form`
+  display: grid;
+  gap: 2em;
+`;
+
+const FormItemStyled = styled.div`
+  position: relative;
+  width: 280px;
+`;
+
+const FormErrorStyled = styled.div`
+  position: absolute;
+  top: 44px;
+  left: 12px;
+  color: red;
+  font-size: x-small;
+`;
+
+const schema: yup.ObjectSchema<Inputs> = yup
+  .object({
+    email: yup.string().required().email(),
+    password: yup.string().required(),
+  })
+  .required();
+
+const FormLogin = ({ t }: { t?: (v: string) => ReactNode | string }) => {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    control,
+    formState: { errors, isValid, isSubmitted },
+  } = useForm<Partial<Inputs>>({
+    // TODO: избавиться от Partial
+    mode: 'onSubmit',
+    defaultValues: { email: '', password: '' },
+    resolver: yupResolver<Inputs>(schema),
+  });
+
+  const customHandleSubmit = (values: Inputs) => {
+    console.log('values: ', values);
+    reset();
+  };
+
+  return (
+    <FormProfileStyled onSubmit={handleSubmit(customHandleSubmit)}>
+      <FormItemStyled className="form--item">
+        <Controller
+          name="email"
+          control={control}
+          render={({ field }) => <Input placeholder={t('form.email') as string} required {...field} />}
+        />
+        {errors.email && <FormErrorStyled className="form--error">{errors.email?.message}</FormErrorStyled>}
+      </FormItemStyled>
+
+      <FormItemStyled className="form--item">
+        <Controller
+          name="password"
+          control={control}
+          render={({ field }) => <Input placeholder={t('form.password') as string} required {...field} />}
+        />
+        {errors.password && <FormErrorStyled className="form--error">{errors.password?.message}</FormErrorStyled>}
+      </FormItemStyled>
+
+      <input className="button button--primary button--medium" type="submit" />
+    </FormProfileStyled>
+  );
+};
+
+const FormLoginTranslated = withTranslation('common')(FormLogin);
+export default FormLoginTranslated;
