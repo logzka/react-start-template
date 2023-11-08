019ad0a1582a220e6644170728840b7ade0755e3
petruha <refloader@gmail.com>
2023-11-08 13:23:04 +0300

    Add storybook translation
    https://storybook.js.org/addons/storybook-i18next

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,13 +1,23 @@
-import type { Preview } from "@storybook/react";
+import type { Preview } from '@storybook/react';
+
+import i18n from './i18next';
 
 const preview: Preview = {
   parameters: {
-    actions: { argTypesRegex: "^on[A-Z].*" },
+    i18n,
+    actions: { argTypesRegex: '^on[A-Z].*' },
     controls: {
       matchers: {
         color: /(background|color)$/i,
         date: /Date$/,
       },
+    },
+  },
+  globals: {
+    locale: 'ru',
+    locales: {
+      en: { title: 'English', left: '🇺🇸' },
+      ru: { title: 'Russian', left: '🇷🇺' },
     },
   },
 };
