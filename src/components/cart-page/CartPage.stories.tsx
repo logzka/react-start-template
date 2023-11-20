0db98f310ea88e703b0d72e51b9093fd581f8f65
petruha <refloader@gmail.com>
2023-11-20 17:47:14 +0300

    CartPage basic content

--- src/components/cart-page/CartPage.stories.tsx
+++ src/components/cart-page/CartPage.stories.tsx
@@ -1,4 +1,5 @@
 import CartPageTranslated from './CartPage';
+import { cakes as cakesData } from '../../api/cakes';
 
 export default {
   title: 'Pages/CartPage',
@@ -7,14 +8,6 @@ export default {
 
 export const DefaultCartPage = {
   args: {
-    // categoryName: 'Торты',
-    // name: 'Малиновый чизкейк',
-    // price: '1299',
-    // priceOld: '1300',
-    // description:
-    //   'Популярный десерт с сырной основой и тонким коржом из песочного теста с добавлением малинового пюре. Нежное творожное суфле, дополненное тонкой бисквитной прослойкой, прекрасное сочетание для любителей классики. В составе только натуральные молочные продукты. Без добавления растительных жиров.',
-    // imageUrls: [
-    //   'https://shop-cdn1-2.vigbo.tech/shops/218522/products/21922955/images/3-9b48c64af285a36ca7de4fbc2d490de1.jpg',
-    // ],
+    cakes: cakesData,
   },
 };
--- src/components/cart-page/CartPage.tsx
+++ src/components/cart-page/CartPage.tsx
@@ -1,11 +1,56 @@
 import React, { FC, ReactNode } from 'react';
 import { withTranslation } from 'react-i18next';
+import { styled } from 'styled-components';
+import Button from '../button/Button';
+
+/** Components */
+import CartItem from '../cart-item/CartItem';
 
 interface tr {
   t?: (v: string) => ReactNode | string;
+  cakes: [];
 }
 
-const CartPage: FC<tr> = ({ t }) => <p>{t('Страница корзины')}</p>;
+const ItemList = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 2em;
+  padding: 2em 0;
+`;
+
+const ItemWrapper = styled.div`
+  width: 100%;
+  /* display: grid; */
+  justify-content: center;
+  gap: 2em;
+  /* grid-template-columns: repeat(auto-fill, minmax(320px, 320px)); */
+`;
+
+const CartPage: FC<tr> = ({ cakes, t }) => {
+  return (
+    <>
+      <ItemList>
+        <ItemWrapper>
+          {cakes.map(({ categoryName, name, price, priceOld, description, imageUrl, id }) => (
+            <CartItem
+              key={id}
+              type="default"
+              categoryName={categoryName}
+              name={name}
+              price={price}
+              priceOld={priceOld}
+              description={description}
+              imageUrls={[imageUrl]}
+            />
+          ))}
+        </ItemWrapper>
+      </ItemList>
+      <Button>{t('Оформить заказ')}</Button>
+    </>
+  );
+};
 
 const CartPageTranslated = withTranslation('common')(CartPage);
 export default CartPageTranslated;
