14e37f23cc6eb0f0c846c74cf8f5df8da57e49d3
logzka <106141584+logzka@users.noreply.github.com>
2024-01-06 09:51:18 +0000

    Deploying to gh-pages from @ logzka/react-start-template@aeaacc42688428ae2fc18bda2821deeae4f59efb 🚀

--- storybook/components/cart-page/types.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from 'react';
-
-/** Types */
-import { TCake } from '../../api/cakes';
-
-export interface ICartPageProps {
-  t?: (v: string, params?: any) => ReactNode | string;
-  cartCakes: TCake[];
-}
