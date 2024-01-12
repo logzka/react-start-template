6b90b1ddcc0b59d7c3f813412ef8ee5f49d6b816
petruha <refloader@gmail.com>
2024-01-12 17:16:07 +0300

    Add login mutation

--- src/pages/auth/LoginPage.tsx
+++ src/pages/auth/LoginPage.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
+import { useMutation, useLazyQuery } from '@apollo/client';
 
 /** Components */
 import FormLoginTranslated from 'src/components/form/form-login/FormLogin';
@@ -9,10 +10,14 @@ import { profileAdd, profileReset } from 'src/redux/profileReducer';
 import { tokenThunks, tokenSelectors } from 'src/redux/tokenReducer';
 import { RootState, useTypedDispatch } from 'src/store';
 
+/** GQL */
+import { LOGIN } from 'src/graphql/schemes/LOGIN';
+
 const LoginPage = () => {
   const dispatch = useTypedDispatch();
   const { resetTokenThunk } = tokenThunks;
   const token = useSelector<RootState, RootState['token']>(tokenSelectors.get);
+  const [login, { error }] = useMutation(LOGIN);
 
   useEffect(() => {
     if (token) {
@@ -26,21 +31,41 @@ const LoginPage = () => {
   const goToHome = () => navigate('/');
 
   function onSubmit(event: Inputs) {
-    dispatch(tokenThunks.setTokenThunk(event.email));
-    /** Set fake user data */
-    dispatch(
-      profileAdd({
-        firstName: 'Administrator',
-        lastName: '',
-        gender: 'male',
-        age: 30,
-        phone: '79609999999',
-        email: event.email,
-        role: 'admin',
+    login({ variables: event, errorPolicy: 'none' })
+      .then((res) => {
+        const {
+          data: {
+            profile: {
+              signin: { token },
+            },
+          },
+        } = res;
+
+        dispatch(tokenThunks.setTokenThunk(token));
+
+        /** Set fake user data */
+        dispatch(
+          profileAdd({
+            firstName: 'Administrator',
+            lastName: '',
+            gender: 'male',
+            age: 30,
+            phone: '79609999999',
+            email: event.email,
+            role: 'admin',
+          })
+        );
+        goToHome();
       })
-    );
-    goToHome();
+      .catch((err) => {
+        // TODO: показывать уведомление, например, или вкорачить в ошибки формы
+        console.log(err);
+      });
   }
+
+  // if (loading) return 'Submitting...';
+  // if (error) console.log(error);
+  // if (error) return `Submission error! ${error.message}`;
 
   return <FormLoginTranslated onSubmitHandler={onSubmit} />;
 };
