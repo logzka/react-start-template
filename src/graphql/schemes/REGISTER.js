ecbe4dc5771c945c51656058e976a7a27107bde3
petruha <refloader@gmail.com>
2024-01-14 00:14:13 +0300

    Add signup page

---
+++ src/graphql/schemes/REGISTER.js
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+export const REGISTER = gql`
+  mutation Register($email: String!, $password: String!) {
+    profile {
+      signup(email: $email, password: $password, commandId: "cakeshop") {
+        token
+      }
+    }
+  }
+`;
