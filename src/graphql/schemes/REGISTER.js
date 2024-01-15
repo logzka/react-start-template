29a67a89f94a5ca5c98f6adc0a76be514b606b01
Anastasiia Davydova <logzkada@gmail.com>
2024-01-15 11:35:31 +0200

    ORST-20 Removed CommandId from register gql scheme.

--- src/graphql/schemes/REGISTER.js
+++ src/graphql/schemes/REGISTER.js
@@ -3,7 +3,7 @@ import { gql } from '@apollo/client';
 export const REGISTER = gql`
   mutation Register($email: String!, $password: String!) {
     profile {
-      signup(email: $email, password: $password, commandId: "cakeshop") {
+      signup(email: $email, password: $password) {
         token
       }
     }
